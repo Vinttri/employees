507d21bfe98b5d14dd0acc6d26b78633308fb625
Vinttri <vint.tri@gmail.com>
2026-08-06 10:09:38 +0300

    fix: include Collectives with nullable trash state

--- lib/Service/DirectorySyncService.php
+++ lib/Service/DirectorySyncService.php
@@ -367,7 +367,10 @@ class DirectorySyncService {
 			$result = $qb->select('c.circle_unique_id', 'circle.name')
 				->from('collectives', 'c')
 				->innerJoin('c', 'circles_circle', 'circle', $qb->expr()->eq('circle.unique_id', 'c.circle_unique_id'))
-				->where($qb->expr()->eq('c.trash_timestamp', $qb->createNamedParameter(0, IQueryBuilder::PARAM_INT)))
+				->where($qb->expr()->orX(
+					$qb->expr()->isNull('c.trash_timestamp'),
+					$qb->expr()->eq('c.trash_timestamp', $qb->createNamedParameter(0, IQueryBuilder::PARAM_INT)),
+				))
 				->executeQuery();
 			foreach ($result->fetchAll() as $row) {
 				$id = trim((string)($row['circle_unique_id'] ?? ''));
